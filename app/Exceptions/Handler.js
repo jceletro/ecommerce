ba7ab3bc1481e6c7324d2d5bd1cdc764c6bf206f
kelvynsantana <santanakelvyn@gmail.com>
2020-06-09 07:09:23 -0300

    Refatoring Error Handling

--- app/Exceptions/Handler.js
+++ app/Exceptions/Handler.js
@@ -1,5 +1,6 @@
 'use strict'
-
+const Env = use('Env')
+const Youch = use('youch')
 const BaseExceptionHandler = use('BaseExceptionHandler')
 
 /**
@@ -23,6 +24,12 @@ class ExceptionHandler extends BaseExceptionHandler {
   async handle(error, { request, response }) {
     if (error.name === 'ValidationException') {
       return response.status(error.status).send(error.messages)
+    }
+    if(Env.get('NODE_ENV') === 'development') {
+      const youch = new Youch(error, request.request)
+      const errorJSON = await youch.toJSON()
+
+      return response.status(error.status).send(errorJSON)
     }
     return response.status(error.status)
   }
